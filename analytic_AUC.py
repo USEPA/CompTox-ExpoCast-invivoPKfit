f333e3a0787082d797eb8b67d9caa9df20d0946e
cring <Ring.Caroline@epa.gov>
2022-11-28 12:55:28 -0500

    Update python script that uses SymPy to calculate analytic AUCs, to reflect fixes to 2-compartment oral model

--- analytic_AUC.py
+++ analytic_AUC.py
@@ -7,53 +7,58 @@ Created on Thu Nov 17 14:37:44 2022
 
 from sympy import *
 
-#D = dose
-#t = time
-#T = end time
-#Assumption: D and t are nonnegative
-D, t = symbols('D, t', nonnegative = True)
-#Assumption: T is positive
-T = symbols('T', positive = True)
+#Dose = dose
+#Time = Time
+#T_end = end Time
+#Assumption: Dose and Time are nonnegative
+Dose, Time = symbols('Dose, Time', nonnegative = True)
+#Assumption: T_end is positive
+T_end = symbols('T_end', positive = True)
 
 # One compartment model analytic AUC
 ## Parameters:
-    ## ke = Elimination rate
-    ## Vd = Volume of distribution
+    ## kelim = Elimination rate
+    ## Vdist = Volume of distribution
     ## Assumption: Both of these are positive
-ke, Vd= symbols('ke, Vd', positive = True)
+kelim, Vdist= symbols('kelim, Vdist', positive = True)
 ## IV dose
-### Cp(t)
-comp1_iv = (D/Vd)*exp(-ke*t)
-### Integrate from 0 to T
-integrate(comp1_iv, (t, 0, T))
-### Result:D/(Vd*ke) - D*exp(-T*ke)/(Vd*ke)
-
+### Cp(Time)
+comp1_iv = (Dose/Vdist)*exp(-kelim*Time)
+### Integrate from 0 to T_end
+integrate(comp1_iv, (Time, 0, T_end))
+### Result: Dose/(Vdist*kelim) - Dose*exp(-T_end*kelim)/(Vdist*kelim)
 
 ##Oral dose
-ka = symbols('ka', positive = True)
-Fa = symbols('Fa', nonnegative = True)
-comp1_po = (D*Fa*ka)/(Vd*(ka - ke)) * (exp(-ke*t)- exp(-ka*t))
-integrate(comp1_po, (t, 0, T))
-### Result: -D*Fa*ka*(-1/ke + 1/ka)/(Vd*(ka - ke)) + D*Fa*ka*(-exp(-T*ke)/ke + exp(-T*ka)/ka)/(Vd*(ka - ke))
+kgutabs = symbols('kgutabs', positive = True)
+Fgutabs = symbols('Fgutabs', nonnegative = True)
+comp1_po = (Dose*Fgutabs*kgutabs)/(Vdist*(kgutabs - kelim)) * (exp(-kelim*Time)- exp(-kgutabs*Time))
+integrate(comp1_po, (Time, 0, T_end))
+### Result: -Dose*Fgutabs*kgutabs*(1/kgutabs - 1/kelim)/(Vdist*(-kelim + kgutabs)) + Dose*Fgutabs*kgutabs*(exp(-T_end*kgutabs)/kgutabs - exp(-T_end*kelim)/kelim)/(Vdist*(-kelim + kgutabs))
 
-### Case when kelim = ka
-comp1_po_alt = (D*Fa*ke)/Vd *t *exp(-ke*t)
-integrate(comp1_po_alt, (t, 0, T))
-### Result: D*Fa/(Vd*ke) + (-D*Fa*T*ke - D*Fa)*exp(-T*ke)/(Vd*ke)
+### Case when kelim = kgutabs
+comp1_po_alt = (Dose*Fgutabs*kelim)/Vdist *Time *exp(-kelim*Time)
+integrate(comp1_po_alt, (Time, 0, T_end))
+### Result: Dose*Fgutabs/(Vdist*kelim) + (-Dose*Fgutabs*T_end*kelim - Dose*Fgutabs)*exp(-T_end*kelim)/(Vdist*kelim)
 
 ## Two compartment model analytic AUC
 V1 = symbols('V1', positive = True)
 alpha, beta = symbols('alpha, beta', positive = True)
 k21, k12 = symbols('k21, k12', positive = True)
 A, B = symbols('A, B', positive = True)
-A_exp = D*(alpha - k21)/(V1*(alpha - beta))
-B_exp = D*(k21 - beta)/(V1*(alpha - beta))
-comp2_iv = A*exp(-alpha * t) + B * exp(-beta*t)
-integrate(comp2_iv, (t, 0, T))
-### A/alpha - A*exp(-T*alpha)/alpha + B/beta - B*exp(-T*beta)/beta
 
-comp2_po = A * exp(-alpha * t) + B * exp(-beta * t) + -(A+B) * exp(-ka * t)
-integrate(comp2_po, (t, 0, T))
-### Result:A/alpha - A*exp(-T*alpha)/alpha + B/beta - B*exp(-T*beta)/beta + (-A - B)/ka - (-A - B)*exp(-T*ka)/ka
+### IV data
+A_exp = Dose*(alpha - k21)/(V1*(alpha - beta))
+B_exp = Dose*(k21 - beta)/(V1*(alpha - beta))
+comp2_iv = A*exp(-alpha * Time) + B * exp(-beta*Time)
+integrate(comp2_iv, (Time, 0, T_end))
+### A/alpha - A*exp(-T_end*alpha)/alpha + B/beta - B*exp(-T_end*beta)/beta
+
+### Oral data
+A_exp_po = (kgutabs * Fgutabs/V1 * Dose * (alpha - k21)) / ( (kgutabs - alpha) * (alpha - beta))
+B_exp_po = (ka * Fgutabs/V1 * Dose * (k21 - beta)) / ( (kgutabs - beta) * (alpha - beta))
+
+comp2_po =  A * exp(-alpha * Time) + B * exp(-beta * Time) + -(A + B) * exp(-kgutabs * Time)
+integrate(comp2_po, (Time, 0, T_end))
+### Result: A/alpha - A*exp(-T_end*alpha)/alpha + B/beta - B*exp(-T_end*beta)/beta + (-A - B)/kgutabs - (-A - B)*exp(-T_end*kgutabs)/kgutabs
 
 
