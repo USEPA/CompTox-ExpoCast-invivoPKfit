268db8523e0384a6d0cdd353e15c1f519cbf94c8
cring <Ring.Caroline@epa.gov>
2023-01-17 17:05:26 -0500

    Add functions to calculate RMSE and R-squared, handling summary data properly.

---
+++ invivopkfit/R/calc_rmse.R
@@ -0,0 +1,69 @@
+calc_rmse <- function(group_mean,
+                      group_sd,
+                      group_N,
+                      group_pred,
+                      group_LOQ){
+  #If both obs and pred are below LOQ, set obs to pred.
+  #This will effectively make error zero in these cases.
+  group_mean[group_mean < group_LOQ &
+               group_pred < group_LOQ] <- group_pred[group_mean < group_LOQ &
+                                                       group_pred < group_LOQ]
+
+  #If obs is below LOQ but pred is not, set obs to LOQ.
+  group_mean[group_mean < group_LOQ &
+             !(group_pred < group_LOQ)] <- group_LOQ[group_mean < group_LOQ &
+                                                       !(group_pred < group_LOQ)]
+#Calculate MSE.
+  mse <- (1/sum(group_N)) * sum(
+    (group_N - 1) * group_sd^2 +
+      group_N * group_mean^2 +
+      -2 * group_N * group_mean * group_pred +
+      group_pred^2
+  )
+
+  rmse <- sqrt(mse)
+
+  return(rmse)
+}
+
+calc_rsq <- function(group_mean,
+                     group_sd,
+                     group_N,
+                     group_pred){
+  #If both obs and pred are below LOQ, set obs to pred.
+  #This will effectively make error zero in these cases.
+  group_mean[group_mean < group_LOQ &
+               group_pred < group_LOQ] <- group_pred[group_mean < group_LOQ &
+                                                       group_pred < group_LOQ]
+
+  #If obs is below LOQ but pred is not, set obs to LOQ.
+  group_mean[group_mean < group_LOQ &
+               !(group_pred < group_LOQ)] <- group_LOQ[group_mean < group_LOQ &
+                                                         !(group_pred < group_LOQ)]
+
+  pred_bar <- weighted.mean(group_pred, group_N)
+  x_bar <- weighted.mean(group_mean, group_N)
+#Calculate numerator of Pearson correlation coefficient
+  r_num <- sum(group_pred * group_N * group_mean) -
+    pred_bar * sum(group_N * group_mean) -
+    x_bar * sum(group_N * group_pred) +
+    x_bar * pred_bar * sum(group_N)
+#Calculate first denominator term of Pearson correlation coefficient
+  r_denom_1 <- sqrt(sum(
+    (group_N - 1) * group_sd^2 +
+      group_N * group_mean^2
+  ) -
+    2 * x_bar * sum(group_N * group_mean) +
+    sum(group_N) * x_bar^2)
+  #Calculate second denominator term of Pearson correlation coefficient
+  r_denom_2 <- sqrt(sum(group_N * group_pred^2) -
+    2 * pred_bar * sum(group_N * group_pred) +
+    sum(group_N) * pred_bar^2)
+
+r <- r_num /(r_denom_1 * r_denom_2)
+
+rsq <- r^2
+
+return(rsq)
+
+}
