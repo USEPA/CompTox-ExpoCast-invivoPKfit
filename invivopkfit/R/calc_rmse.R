0b50e78c3dbbdd7ce114971e2f3de888cc2ba467
cring <Ring.Caroline@epa.gov>
2023-01-20 10:16:48 -0500

    Add function to convert natural-scale sample mean and SD to log-scale. Add "log" argument to calc_rmse() and calc_rsq() to handle the case when log conversion is needed (i.e. if fitting was done on log-scaled concentrations).

--- invivopkfit/R/calc_rmse.R
+++ invivopkfit/R/calc_rmse.R
@@ -2,7 +2,8 @@ calc_rmse <- function(group_mean,
                       group_sd,
                       group_N,
                       group_pred,
-                      group_LOQ){
+                      group_LOQ,
+                      log = FALSE){
   #If both obs and pred are below LOQ, set obs to pred.
   #This will effectively make error zero in these cases.
   group_mean[group_mean <= group_LOQ &
@@ -13,6 +14,15 @@ calc_rmse <- function(group_mean,
   group_mean[group_mean <= group_LOQ &
              !(group_pred <= group_LOQ)] <- group_LOQ[group_mean <= group_LOQ &
                                                        !(group_pred <= group_LOQ)]
+
+  #Convert to log-scale if necessary
+  if(log %in% TRUE){
+    tmplist <- convert_summary_to_log(sample_mean = group_mean,
+                                      sample_SD = group_sd)
+    group_mean <- tmplist$logmean
+    group_sd <- tmplist$logSD
+  }
+
 #Calculate MSE.
   mse <- (1/sum(group_N)) * sum(
     (group_N - 1) * group_sd^2 +
@@ -29,7 +39,8 @@ calc_rmse <- function(group_mean,
 calc_rsq <- function(group_mean,
                      group_sd,
                      group_N,
-                     group_pred){
+                     group_pred,
+                     log = FALSE){
   #If both obs and pred are below LOQ, set obs to pred.
   #This will effectively make error zero in these cases.
   group_mean[group_mean <= group_LOQ &
@@ -40,6 +51,14 @@ calc_rsq <- function(group_mean,
   group_mean[group_mean <= group_LOQ &
                !(group_pred <= group_LOQ)] <- group_LOQ[group_mean < group_LOQ &
                                                          !(group_pred < group_LOQ)]
+
+  #Convert to log-scale if necessary
+  if(log %in% TRUE){
+    tmplist <- convert_summary_to_log(sample_mean = group_mean,
+                                      sample_SD = group_sd)
+    group_mean <- tmplist$logmean
+    group_sd <- tmplist$logSD
+  }
 
   pred_bar <- weighted.mean(group_pred, group_N)
   x_bar <- weighted.mean(group_mean, group_N)
@@ -66,4 +85,18 @@ rsq <- r^2
 
 return(rsq)
 
+}
+
+#' Convert sample mean and SD to log-scale
+#'
+#' @param sample_mean Numeric: one or more sample means
+#' @param sample_SD Numeric: one or more sample SDs
+#' @return A list with two named elements: "logmean" and "logSD", the log-scale
+#'   sample means and log-scale sample SDs, respectively.
+#' @author Caroline Ring
+convert_summary_to_log <- function(sample_mean, sample_SD){
+  logmean <- log(sample_mean^2 / sqrt(sample_SD^2 + sample_mean^2))
+  logSD <- sqrt(log(1 + (sample_SD^2 / sample_mean^2)))
+  return(list("logmean" = logmean,
+              "logSD" = logSD))
 }
