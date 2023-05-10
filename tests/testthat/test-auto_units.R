c0827c10cd7e447a7c21da04ef9dcaf2685c9bd4
cring <Ring.Caroline@epa.gov>
2023-05-10 16:06:55 -0400

    auto_units() now finds the units that make the *order of magnitude* of the midpoint of the input time vector closest to 10 (i.e., minimizes the difference between log10 of the midpoint and 1).

---
+++ tests/testthat/test-auto_units.R
@@ -0,0 +1,41 @@
+test_that("time points over 2 days get converted to hours",
+          {
+            test_time <- seq(0,2,by=(1/24))
+            expect_equal(auto_units(y = test_time,
+                                    from = "days"),
+                         "hours")
+          }
+)
+
+test_that("time points over 24 hours stay in hours",
+          {
+            test_time <- seq(0,24)
+            expect_equal(auto_units(y = test_time,
+                                    from = "hours"),
+                         "hours")
+          }
+)
+
+test_that("time points in hours over a month get converted to days",
+          {
+            expect_equal(
+              auto_units(y = convert_time(seq(0,30),
+                                          from = "days",
+                                          to = "hours"),
+                         from = "hours"),
+              "days"
+            )
+          }
+)
+
+test_that("time points in hours over a year get converted to months",
+          {
+            expect_equal(
+              auto_units(y = convert_time(seq(0,365),
+                                          from = "days",
+                                          to = "hours"),
+                         from = "hours"),
+              "months"
+            )
+          }
+)
