aed9479e49b1dca468b8c599e050d5ba6fa271c7
Ring <Ring.Caroline@epa.gov>
2023-06-09 08:36:34 -0400

    Initial implementation of faceting for pk objects. Not yet tested.

--- R/pk_faceted_methods_add_pkproto.R
+++ R/pk_faceted_methods_add_pkproto.R
@@ -1,12 +1,51 @@
-#'add_pkproto
+#' Add a `pkproto` object to a `pk_faceted` object
 #'
-#'add_pkproto for pk_faceted objects
+#' Add a `pkproto` object to a `pk_faceted` object
 #'
-#'@param obj An object of class `pk_faceted`
-#'@return A [tibble::tibble()] grouped by the faceting variables with a new variable ` add_pkproto ` containing the results of applying [add_pkproto.pk()] to each item in the list column of [pk()] objects, `obj$pk_object`
+#' Note that `e1 + e2` is equivalent to
+#'
+#' ```
+#' `+`(e1, e2)
+#' ```
+#'
+#' @param e1 A `pk_faceted` pbject
+#' @param e2 A `pkproto` object
+#' @return The `pk` object, modified by adding the `pkproto` object
 #'@export
-#'@author Caroline Ring
-#'@family methods for pk_faceted objects
-add_pkproto.pk <- function(obj, ...){
-obj %>% dplyr::mutate(add_pkproto =purrr::map(pk_object,add_pkproto))
+#' @author Caroline Ring
+"+.pk_faceted" <- function(e1, e2) {
+  #throw error if only one argument
+  if (missing(e2)) {
+    cli::cli_abort(c(
+      "Cannot use {.code +} with a single argument",
+      "i" = "Did you accidentally put {.code +} on a new line?"
+    ))
+  }
+
+  # Get the name of what was passed in as e2, and pass along so that it
+  # can be displayed in error messages
+  e2name <- deparse(substitute(e2))
+
+  #make sue
+  if      (is.pk_faceted(e1)) {
+    if(!inherits(e2, "facet_data")){
+    #add to each of the pk objects in turn
+    e1 %>% dplyr::transmute(pk_object = purrr::map(pk_object,
+                                                   function(x){
+                                                     add_pk(x, e2, e2name)
+                                                   }))
+    }else{ #can't re-facet already-faceted data.
+      e1name <- deparse(substitute(e1))
+      stop(paste(e1name,
+                 "is already faceted. New faceting",
+                 e2name,
+                 "cannot be applied."))
+    }
+  }
+  else if (is.pkproto(e1)) {
+    cli::cli_abort(c(
+      "Cannot add {.cls pkproto} objects together",
+      "i" = "Did you forget to add this object to a {.cls pk} object?"
+    ))
+  }
 }
