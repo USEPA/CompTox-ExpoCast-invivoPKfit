6812160c11255204c72abf7ecedd62adfd56ab50
Gilberto Padilla Mercado <PadillaMercado.Gilberto@epa.gov>
2025-05-20 09:07:15 -0400

    Updated mapping function to have new CvTdb table column names.

--- R/pkproto_mapping.R
+++ R/pkproto_mapping.R
@@ -6,29 +6,29 @@
 #' @author Caroline Ring
 mapping <- function(
     mapping = ggplot2::aes(
-      Chemical = analyte_dtxsid,
-      Chemical_Name = analyte_name_original,
-      DTXSID = analyte_dtxsid,
-      CASRN = analyte_casrn,
+      Chemical = analyzed_chem_dtxsid,
+      Chemical_Name = analyzed_chem_name_original,
+      DTXSID = analyzed_chem_dtxsid,
+      CASRN = analyzed_chem_casrn,
       Species = species,
-      Reference = document_id,
+      Reference = fk_extraction_document_id,
       Media = conc_medium_normalized,
       Route = administration_route_normalized,
-      Dose = dose_level_corrected,
+      Dose = invivPK_dose_level,
       Dose.Units = "mg/kg",
-      Subject_ID = subject_id,
-      Series_ID = series_id,
-      Study_ID = study_id,
+      Subject_ID = fk_subject_id,
+      Series_ID = fk_series_id,
+      Study_ID = fk_study_id,
       ConcTime_ID = conc_time_id,
-      N_Subjects = n_subjects_in_series,
+      N_Subjects = n_subjects_normalized,
       Weight = weight_kg,
       Weight.Units = "kg",
       Time = time_hr,
       Time.Units = "hours",
-      Value = conc,
+      Value = invivPK_conc,
       Value.Units = "mg/L",
-      Value_SD = conc_sd_normalized,
-      LOQ = loq
+      Value_SD = invivPK_conc_sd,
+      LOQ = invivPK_loq
     ),
 ...
 ) {
